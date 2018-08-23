443aa51a244bcab94e6f4ea52ae8b6f374d52df3
Tarik-INC <tarikesmin@gmail.com>
2018-08-23 16:46:13 -0300

    Adding post request method in 'one day predict' , not tested

--- energy_consumption_flask/app.py
+++ energy_consumption_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json, request
 import consumo_de_energia_KNN
 app = Flask(__name__)
 
@@ -8,14 +8,18 @@ def api_root():
     return 'Welcome'
 
 
-@app.route('/predict/oneday/<int:week_day>/', methods=['GET'])
+@app.route('/predict/oneday/', methods=['POST'])
 def predict_one_day(week_day):
     if not(consumo_de_energia_KNN.model_trained):
         consumo_de_energia_KNN.train_model()
 
-    resp = jsonify(consumo_de_energia_KNN.predict_one_day(week_day))
-    resp.status_code = 200
-    return resp
+    if request.headers['Content-Type'] == 'application/json':
+        int_request_data = int(request.json['day'])
+        resp = jsonify(consumo_de_energia_KNN.predict_one_day(int_request_data))
+        resp.status_code = 200
+        return resp
+    else:
+        return None
 
 
 @app.route('/predict/week/', methods=['GET'])
